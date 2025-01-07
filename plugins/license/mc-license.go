a9001933b3c32ffc8c54eadde06ddd0cc5699ea9
solve <solve.sthor@gmail.com>
2025-01-07 13:08:19 +0100

    chore: added plugin LICENSE, can be used to create MIT license file LICENSE within cwd with automatic year generation and Mattilsynet as copyright holder.

---
+++ plugins/license/mc-license.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Mattilsynet/map-cli/plugins/license/types"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:     "license",
+		Short:   "License plugin",
+		Aliases: []string{"l"},
+	}
+	// TODO: Improvement to add such that we can have different licences produced by arguments or flags
+	create := &cobra.Command{
+		Use:   "create",
+		Short: "Create MIT license file called LICENCE within current directory",
+		Run: func(cmd *cobra.Command, args []string) {
+			createLicenceFile(types.MIT_LICENSE)
+		},
+	}
+	rootCmd.AddCommand(create)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func createLicenceFile(license types.LICENCE) {
+	file, err := os.Create("LICENSE")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(license.GetLicense())
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+	}
+	fmt.Println(license.GetType() + " LICENSE file generated.")
+}
---
+++ plugins/license/types/LICENCE.go
@@ -0,0 +1,6 @@
+package types
+
+type LICENCE interface {
+	GetLicense() string
+	GetType() string
+}
