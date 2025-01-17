9db1caf271060c0d0b1774677a382674d1da9553
Sølve Sthør <solve.rene.sthor@mattilsynet.no>
2025-01-17 15:58:05 +0100

    chore: updated config to read actual nats creds from paths.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 
@@ -44,13 +46,57 @@ func DefaultConfig() *Config {
 func (n *Nats) GetConnection() (*nats.Conn, error) {
 	if n.nc == nil {
 		// TODO: do this more dynamically with using the credentials file path if exists, and determine nats url from config
-		nc, err := nats.Connect("nats://localhost:4222")
+		url, credsFilePath, natsContextErr := extractNatsContext(n.CredentialFilePath)
+		var nc *nats.Conn
+		if natsContextErr != nil {
+			var err error
+			log.Println("No nats context found, connecting to default nats server")
+			nc, err = nats.Connect("nats://localhost:4222")
+			if err != nil {
+				return nil, err
+			}
+			n.nc = nc
+			return n.nc, nil
+
+		}
+		var err error
+		if credsFilePath != "" {
+			log.Println("user creds and url found, connecting")
+			nc, err = nats.Connect(url, nats.UserCredentials(credsFilePath))
+		} else {
+			log.Println("only connecting to url, no nats creds found")
+			nc, err = nats.Connect(url)
+		}
 		if err != nil {
 			return nil, err
 		}
 		n.nc = nc
 	}
 	return n.nc, nil
+}
+
+func extractNatsContext(credentialsFilePath string) (url string, natsCredentialsFilePath string, contextErr error) {
+	content, err := os.Open(credentialsFilePath)
+	if err != nil {
+		return "", "", err
+	}
+	type Config struct {
+		URL   string `json:"url"`
+		Creds string `json:"creds"`
+	}
+
+	// Parse the JSON
+	var config Config
+	if err := json.NewDecoder(content).Decode(&config); err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return "", "", err
+	}
+
+	// Extract the variables
+	// read file from path into string
+	// extract nats url from url in json
+	// extract creds from creds in json
+	return config.URL, config.Creds, nil
 }
 
 func init() {
