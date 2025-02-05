7c75a634ba1f05daea9d5d1b51ff117ba03a13c5
Sølve Sthør <solve.rene.sthor@mattilsynet.no>
2025-02-05 13:49:34 +0100

    chore: added bubbletea and goodness, working on combining text input with selection input, 'boolean'.

---
+++ plugins/component/internal/tui/initprompt.go
@@ -0,0 +1,4 @@
+package tui
+
+func NewComponentPrompt() {
+}
--- plugins/component/mc-component.go
+++ plugins/component/mc-component.go
@@ -3,10 +3,169 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
-	"github.com/Mattilsynet/map-cli/plugins/component/pkg/config"
+	//	"github.com/charmbracelet/bubbles/progress"
+	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/spf13/cobra"
 )
+
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle
+	noStyle             = lipgloss.NewStyle()
+	helpStyle           = blurredStyle
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
+
+	focusedButton = focusedStyle.Render("[ Generate ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Generate"))
+)
+
+type model struct {
+	focusIndex int
+	inputs     []textinput.Model
+	cursorMode cursor.Mode
+}
+
+func initialModel() model {
+	m := model{
+		inputs: make([]textinput.Model, 3),
+	}
+
+	var t textinput.Model
+	for i := range m.inputs {
+		t = textinput.New()
+		t.Cursor.Style = cursorStyle
+		t.CharLimit = 32
+
+		switch i {
+		case 0:
+			t.Placeholder = "Component name"
+			t.Focus()
+			t.PromptStyle = focusedStyle
+			t.TextStyle = focusedStyle
+		case 1:
+			t.Placeholder = "Path to install component, leave blank for current directory"
+			t.CharLimit = 64
+		case 2:
+			t.Placeholder = "Path to install, blank for current directory"
+		}
+
+		m.inputs[i] = t
+	}
+
+	return m
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "esc":
+			return m, tea.Quit
+
+		// Change cursor mode
+		case "ctrl+r":
+			m.cursorMode++
+			if m.cursorMode > cursor.CursorHide {
+				m.cursorMode = cursor.CursorBlink
+			}
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := range m.inputs {
+				cmds[i] = m.inputs[i].Cursor.SetMode(m.cursorMode)
+			}
+			return m, tea.Batch(cmds...)
+
+		// Set focus to next input
+		case "tab", "shift+tab", "enter", "up", "down":
+			s := msg.String()
+
+			// Did the user press enter while the submit button was focused?
+			// If so, exit.
+			if s == "enter" && m.focusIndex == len(m.inputs) {
+				return m, tea.Quit
+			}
+
+			// Cycle indexes
+			if s == "up" || s == "shift+tab" {
+				m.focusIndex--
+			} else {
+				m.focusIndex++
+			}
+
+			if m.focusIndex > len(m.inputs) {
+				m.focusIndex = 0
+			} else if m.focusIndex < 0 {
+				m.focusIndex = len(m.inputs)
+			}
+
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := 0; i <= len(m.inputs)-1; i++ {
+				if i == m.focusIndex {
+					// Set focused state
+					cmds[i] = m.inputs[i].Focus()
+					m.inputs[i].PromptStyle = focusedStyle
+					m.inputs[i].TextStyle = focusedStyle
+					continue
+				}
+				// Remove focused state
+				m.inputs[i].Blur()
+				m.inputs[i].PromptStyle = noStyle
+				m.inputs[i].TextStyle = noStyle
+			}
+
+			return m, tea.Batch(cmds...)
+		}
+	}
+
+	// Handle character input and blinking
+	cmd := m.updateInputs(msg)
+
+	return m, cmd
+}
+
+func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+
+	// Only text inputs with Focus() set will respond, so it's safe to simply
+	// update all of them here without any further logic.
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
+	}
+
+	return tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	var b strings.Builder
+
+	for i := range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
+	}
+
+	button := &blurredButton
+	if m.focusIndex == len(m.inputs) {
+		button = &focusedButton
+	}
+	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
+
+	b.WriteString(helpStyle.Render("cursor mode is "))
+	b.WriteString(cursorModeHelpStyle.Render(m.cursorMode.String()))
+	b.WriteString(helpStyle.Render(" (ctrl+r to change style)"))
+
+	return b.String()
+}
 
 func main() {
 	rootCmd := &cobra.Command{
@@ -14,16 +173,19 @@ func main() {
 		Short:   "Component plugin",
 		Aliases: []string{"c"},
 	}
-
 	generate := &cobra.Command{
 		Use:     "generate",
 		Short:   "Generate a WasmCloud component",
 		Aliases: []string{"gen", "g"},
 		Run: func(cmd *cobra.Command, args []string) {
-			componentConfig := promptUser()
+			_, err := tea.NewProgram(initialModel()).Run()
+			if err != nil {
+				fmt.Println("error starting program:", err)
+				os.Exit(1)
+			}
 			// cue validate schema
-			componentProject := config.CreateComponentProject(componentConfig)
-			config.GenerateFilesPrompt(componentProject)
+			// componentProject := config.CreateComponentProject(componentConfig)
+			// config.GenerateFilesPrompt(componentProject)
 		},
 	}
 	rootCmd.AddCommand(generate)
@@ -31,14 +193,4 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func promptUser() string {
-	// component-name
-	// root-path
-	// nats-core
-	// nats-jetstream
-	// nats-kv
-	// license
-	panic("unimplemented")
 }
